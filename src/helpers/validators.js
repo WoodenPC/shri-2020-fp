a4859e4a059425480ad46e9b1fae4f7b9fed2f57
Arslanov Rasul <stiflerovich@mail.ru>
2020-04-09 16:44:40 +0500

    проверки для 1ого задания

--- src/helpers/validators.js
+++ src/helpers/validators.js
@@ -13,38 +13,97 @@
  * Если какие либо функции написаны руками (без использования библиотек) это не является ошибкой
  */
 
-// 1. Красная звезда, зеленый квадрат, все остальные белые.
-export const validateFieldN1 = ({star, square, triangle, circle}) => {
-    if (triangle !== 'white' || circle !== 'white') {
-        return false;
-    }
+import { pipe, last, allPass, pickBy, propEq, reject, gte,
+    length, props, apply, keys, values, countBy, identity, equals, __,
+    difference, all, complement } from 'ramda';
 
-    return star === 'red' && square === 'green';
+// 1. Красная звезда, зеленый квадрат, все остальные белые.
+export const validateFieldN1 = (shapes) => {
+    const hasWhiteTriangle = propEq('triangle', 'white');
+    const hasWhiteCircle = propEq('circle', 'white');
+    const hasRedStar = propEq('star', 'red');
+    const hasGreenSquare = propEq('square', 'green');
+
+    return allPass([hasWhiteTriangle, hasWhiteCircle, hasRedStar, hasGreenSquare])(shapes);
 };
 
 // 2. Как минимум две фигуры зеленые.
-export const validateFieldN2 = () => false;
+export const validateFieldN2 = (shapes) => {
+    const isGreen = (value) => value === 'green';
+    const greenShapes = pickBy(isGreen, shapes);
+    return keys(greenShapes).length >= 2;
+};
 
 // 3. Количество красных фигур равно кол-ву синих.
-export const validateFieldN3 = () => false;
+export const validateFieldN3 = (shapes) => {
+    const isRed = (value) => value === 'red';
+    const isBlue = (value) => value === 'blue';
+    const redShapes = pickBy(isRed, shapes);
+    const blueShapes = pickBy(isBlue, shapes);
+    return keys(redShapes).length === keys(blueShapes).length;
+};
 
 // 4. Синий круг, красная звезда, оранжевый квадрат
-export const validateFieldN4 = () => false;
+export const validateFieldN4 = (shapes) => {
+    const hasBlueCircle = propEq('circle', 'blue');
+    const hasRedStar = propEq('star', 'red');
+    const hasOrangeSquare = propEq('square', 'orange');
+    return allPass([hasBlueCircle, hasRedStar, hasOrangeSquare])(shapes);
+};
 
 // 5. Три фигуры одного любого цвета кроме белого.
-export const validateFieldN5 = () => false;
+export const validateFieldN5 = (shapes) => {
+    const isWhite = (value) => value === 'white';
+    return pipe(
+        values,
+        reject(isWhite),
+        countBy(identity),
+        values,
+        last,
+        gte(__, 3)
+    )(shapes);
+};
 
 // 6. Две зеленые фигуры (одна из них треугольник), еще одна любая красная.
-export const validateFieldN6 = () => false;
+export const validateFieldN6 = (shapes) => {
+    const hasGreenTriangle = propEq('triangle', 'green');
+    const hasTwoGreenAndRed = pipe(
+        values,
+        difference(['green', 'green', 'red'], __),
+        length,
+        equals(__, 0)
+    );
+    return allPass([hasGreenTriangle, hasTwoGreenAndRed])(shapes);
+};
 
 // 7. Все фигуры оранжевые.
-export const validateFieldN7 = () => false;
+export const validateFieldN7 = (shapes) => {
+    const isOrange = (value) => value === 'orange';
+    return pipe(
+        values,
+        all(isOrange)
+    )(shapes);
+};
 
 // 8. Не красная и не белая звезда.
-export const validateFieldN8 = () => false;
+export const validateFieldN8 = (shapes) => {
+    const hasRedStar = propEq('star', 'red');
+    const hasWhiteStar = propEq('star', 'white');
+    return allPass([complement(hasRedStar), complement(hasWhiteStar)])(shapes);
+};
 
 // 9. Все фигуры зеленые.
-export const validateFieldN9 = () => false;
+export const validateFieldN9 = (shapes) => {
+    const isGreen = (value) => value === 'green';
+    return pipe(
+        values,
+        all(isGreen)
+    )(shapes);
+};
 
 // 10. Треугольник и квадрат одного цвета (не белого)
-export const validateFieldN10 = () => false;
+export const validateFieldN10 = (shapes) => {
+    const hasWhiteSquare = propEq('square', 'white');
+    const triangleEqualsSquare = pipe(props(['triangle', 'square']), apply(equals));
+    return allPass([triangleEqualsSquare, complement(hasWhiteSquare)])(shapes);
+};
